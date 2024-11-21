0ddfb1a8ff89471cf4a907f83aca955231274d44
piotr.sokolowski <piotrsokolowski@outlook.com>
2024-11-21 12:18:20 +0100

    fused both functions for checking codes, added comments

--- comorbidity_score_calc/comorbidity_score_calc/calc.py
+++ comorbidity_score_calc/comorbidity_score_calc/calc.py
@@ -23,62 +23,34 @@ from pathlib import Path
 # Uncomment the following for interactive environments (e.g., Jupyter, VSCode):
 file_path = '/Users/piotr/Documents/GitHub/cci/comorbidity_score_calc/comorbidity_score_calc/codes.json'
 with open(file_path, 'r') as file:
-    mappingdata = json.load(file)
+    __mappingdata = json.load(file)
 
 # Fetch the mapping names from the JSON data
-mapping_names = list(mappingdata.keys())
+# This is later used for checking if the input mapping is one of the available mappings
+__mapping_names = list(__mappingdata.keys())
 
-def check_codes_exact(icd_codes: Union[str, List[str]], code_group: dict) -> bool:
-    """
-        Checks if the codes in 'icd_codes' are present in 'code_group' based on the specified condition.
-        
-        Parameters
-        ----------
-        icd_codes : Union[str, List[str]]
-            A single ICD code or a list of ICD codes to check against the code group.
-        code_group : dict
-            A dictionary containing a "condition" key (which can be 'any' or 'both') and a list of 'codes' or 'code groups' to check.
-            The structure of 'code_group' is:
-                {
-                    "condition": "any" | "both",
-                    "codes": [list of codes or groups of codes]
-                }
-            
-        Returns
-        -------
-        bool:
-            True if the condition is met (either 'any' or 'both'), False otherwise.
-        
-        The 'any' condition returns True if any code in 'icd_codes' is present in the 'codes' list.
-        The 'both' condition returns True only if codes from each subgroup within 'codes' are present in 'icd_codes'.
-    """
 
-    # Ensure icd_codes is a list
-    if isinstance(icd_codes, str):
-        icd_codes = [icd_codes]
-    
-    # Ensure icd_codes contains codes in upper case:
-    icd_codes = [code.upper() for code in icd_codes]
-        
-    condition = code_group["condition"]
-    codes = code_group["codes"]
-    
-    if condition == "any":
-        return any(code in icd_codes for code in codes)
-    elif condition == "both":
-        return all(any(code in icd_codes for code in group) for group in codes)
-    return False
-
-def check_codes_startswith(icd_codes: Union[str, List[str]], code_group: dict) -> bool:
+def __check_codes(icd_codes: Union[str, List[str]], code_group: dict, exact_codes: bool) -> bool:
     """
-        Checks if any codes in 'icd_codes' start with a prefix from the 'code_group' based on the specified condition.
+        # A helper function
+
+        Checks if any codes in 'icd_codes' ((the input codes)) correspond to a code in the 'code_group' ((from the list in the script's JSON file)) based on the specified condition.
+        There are two possible ways the input codes are processed and checked:
+            1. The first way is the more intuitive one and checks for exact matches, i.e. an input code K70.1 will only score if
+               there is K70.1 in the selected mapping.
+            2. The other way is checking for prefixes. This means that if only K70 is present in the script's library, all its 
+               extensions/subcodes will also score, like K70.1, K70.99, etc. This is meant for the situations where additional codes
+               are added to the ICD Codes list and coded for a patient but are not yet implemented in this script's library.
+               Theoretically, this should not lead to any discrepancies in the final score, as the superior codes are only there in
+               the library if all the subcodes are also meant to score.
+               For the above reasons this is the default way the script works and if the user
 
         Parameters
         ----------
         icd_codes : Union[str, List[str]]
-            A single ICD code or a list of ICD codes to check for prefix matches.
+            A single ICD code or a list of ICD codes to check for matches.
         code_group : dict
-            A dictionary containing a "condition" key (either 'any' or 'both') and a list of 'codes' or 'code groups' to check for prefix matches.
+            A dictionary containing a "condition" key (either 'any' or 'both') and a list of 'codes' or 'code groups' to check for matches.
             The structure of 'code_group' is:
                 {
                     "condition": "any" | "both",
@@ -90,8 +62,8 @@ def check_codes_startswith(icd_codes: Union[str, List[str]], code_group: dict) -
         bool:
             True if the condition is met ('any' or 'both'), False otherwise.
 
-        The 'any' condition returns True if any of the codes in 'icd_codes' starts with any prefix from the 'codes' list.
-        The 'both' condition returns True only if there is at least one matching prefix in each subgroup of the 'codes' list.
+        The 'any' condition returns True if any of the codes in 'icd_codes' starts with any, or matches directly, a code from the 'codes' list.
+        The 'both' condition returns True only if there is at least one matching prefix, or exact code, in each subgroup of the 'codes' list.
     """
 
     # Ensure icd_codes is a list
@@ -104,13 +76,35 @@ def check_codes_startswith(icd_codes: Union[str, List[str]], code_group: dict) -
     condition = code_group["condition"]
     codes = code_group["codes"]
 
-    if condition == "any":
-        # Check if any of the codes in icd_codes start with any of the prefixes in codes
-        return any(code.startswith(prefix) for code in icd_codes for prefix in codes)
-    elif condition == "both":
-        # Check if both conditions are met in case of grouped codes
-        return all(any(code.startswith(prefix) for code in icd_codes) for group in codes for prefix in group)
-    return False
+    # Now according to the parameter exact_codes, check for either exact codes od prefixes (see explanation in the function's docstring)
+    if exact_codes:
+        if condition == "any":
+            return any(code in icd_codes for code in codes)
+            # Explanation of the above logic:
+            # The above will return True, if any ICD Code ((variable: code)) from the selected 
+            # category in the library ((variable: codes)) is present in the input ICD Codes ((variable: icd_codes))
+
+        elif condition == "both":
+            return all(any(code in icd_codes for code in group) for group in codes)
+            # Explanation of the above logic:
+            # The difference to the previous logic is, that now there are separate groups WITHIN a category of ICD Codes in the library,
+            # and we need to check them separately , because only if the codes from BOTH groups are present in the input,
+            # will the category score.
+            # The any() function: any(code in icd_codes for code in group) is already explained above, 
+            # Now this function is used to iterate over all groups of the selected category: (any(...) for group in codes)
+            # and returns as many boolean values as there are groups within a category.
+            # The outermost function all(...) makes sure the category scores only if all the booleans are True
+        return False # If none of the codes was found to match, return False
+
+    else:    
+    # The logic in this case is analog to the above with the difference of checking for prefixes with the help of .startswith() method
+        if condition == "any":
+            # Check if any of the codes in the input icd_codes start with any of the prefixes in codes
+            return any(code.startswith(prefix) for code in icd_codes for prefix in codes)
+        elif condition == "both":
+            # Check if both conditions are met in case of grouped codes
+            return all(any(code.startswith(prefix) for code in icd_codes) for group in codes for prefix in group)
+        return False
 
 def calculate_score(*, icd_codes: Union[str, list], mapping:str = "cci_icd2024gm", exact_codes:bool = False) -> tuple:
     '''
@@ -149,13 +143,13 @@ def calculate_score(*, icd_codes: Union[str, list], mapping:str = "cci_icd2024gm
     '''
     
     # Check if the mapping name (input as the "mapping" argument) is available and raise 
-    if mapping not in mapping_names:
+    if mapping not in __mapping_names:
         raise ValueError(
-            f"Invalid mapping '{mapping}'. Available mappings are: {', '.join(mapping_names)}"
+            f"Invalid mapping '{mapping}'. Available mappings are: {', '.join(__mapping_names)}"
             )
 
     # Assuming correct mapping: According to the value of the 'mapping' argument, choose the right mapping
-    data = mappingdata[mapping]
+    data = __mappingdata[mapping]
 
     # Validate input type
     ## The possible input is either a string when only one ICD Code is given or a list of strings when multiple codes are given
@@ -171,18 +165,12 @@ def calculate_score(*, icd_codes: Union[str, list], mapping:str = "cci_icd2024gm
     # Iterate over all the categories to check if any of the codes in a given category is in the input using either the prefix-based matching
     # or exact codes, as according to the exact_codes parameter
 
-    if exact_codes == True:
-        for category, details in data.items():
-            for code_group in details["codes"]:
-                if check_codes_exact(icd_codes, code_group):
-                    scored_categories.add(category)
-                    break  # Stop checking further groups in this category since it has already scored
-    else:
-        for category, details in data.items():
-            for code_group in details["codes"]:
-                if check_codes_startswith(icd_codes, code_group):
-                    scored_categories.add(category)
-                    break  # Stop checking further groups in this category since it has already scored
+    for category, details in data.items():
+        for code_group in details["codes"]:
+            if __check_codes(icd_codes, code_group, exact_codes = exact_codes):
+                scored_categories.add(category)
+                break  # Stop checking further groups in this category since it has already scored
+
 
     # Handle dependencies and adjust the scored categories accordingly
     ## There are specific hierarchies in the scoring mechanism of this comorbidity index for the case of less and more severe illnesses,
@@ -210,4 +198,4 @@ def calculate_score(*, icd_codes: Union[str, list], mapping:str = "cci_icd2024gm
     # Return the score and the list of scored categories
     return score, list(scored_categories)
 
-calculate_score(icd_codes = [], mapping = 'cci_icd2024gm')
+calculate_score(icd_codes = ['K70.11'], mapping = 'cci_icd2024gm', exact_codes=False)
